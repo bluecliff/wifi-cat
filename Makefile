b5ef31738cf15c943f5f372a0db93d0abbbf5317
li0shuangjiang <li0shuangjiang@gmail.com>
2014-11-12 18:24:18 +0800

    init wificat src

---
+++ Makefile
@@ -0,0 +1,65 @@
+
+CC?=gcc
+CFLAGS?=-O2
+CFLAGS+=-Isrc -Ilibhttpd
+#CFLAGS+=-Wall -Wwrite-strings -pedantic -std=gnu99
+LDFLAGS+=-pthread
+LDLIBS=
+
+NDS_OBJS=src/auth.o src/client_list.o src/commandline.o src/conf.o \
+	src/debug.o src/firewall.o src/fw_iptables.o src/gateway.o src/http.o \
+	src/httpd_handler.o src/ndsctl_thread.o src/safe.o src/tc.o src/util.o
+
+LIBHTTPD_OBJS=libhttpd/api.o libhttpd/ip_acl.o \
+	libhttpd/protocol.o libhttpd/version.o
+
+.PHONY: all clean install checkastyle fixstyle
+
+all: nodogsplash ndsctl
+
+%.o : %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+nodogsplash: $(NDS_OBJS) $(LIBHTTPD_OBJS)
+	$(CC) $(LDFLAGS) -o nodogsplash $+ $(LDLIBS)
+
+ndsctl: src/ndsctl.o
+	$(CC) $(LDFLAGS) -o ndsctl $+ $(LDLIBS)
+
+clean:
+	rm -f nodogsplash ndsctl src/*.o libhttpd/*.o
+	rm -rf dist
+
+install:
+	strip nodogsplash
+	strip ndsctl
+	mkdir -p $(DESTDIR)/usr/bin/
+	cp ndsctl $(DESTDIR)/usr/bin/
+	cp nodogsplash $(DESTDIR)/usr/bin/
+	mkdir -p $(DESTDIR)/etc/nodogsplash/htdocs/images
+	cp resources/nodogsplash.conf $(DESTDIR)/etc/nodogsplash/
+	cp resources/splash.html $(DESTDIR)/etc/nodogsplash/htdocs/
+	cp resources/infoskel.html $(DESTDIR)/etc/nodogsplash/htdocs/
+	cp resources/splash.jpg $(DESTDIR)/etc/nodogsplash/htdocs/images/
+
+checkastyle:
+	@command -v astyle >/dev/null 2>&1 || \
+	{ echo >&2 "We need 'astyle' but it's not installed. Aborting."; exit 1; }
+
+fixstyle: checkastyle
+	@echo "\033[1;34mChecking style ...\033[00m"
+	@astyle --lineend=linux --suffix=none --style=kr --indent=force-tab \
+	--formatted --recursive "src/*.c" "src/*.h"|grep formatted \
+	&& echo "\033[1;33mPrevious files have been corrected\033[00m" \
+	|| echo "\033[0;32mAll files are ok\033[00m"
+
+deb:
+	mkdir -p dist/nodogsplash/
+	cd dist/nodogsplash/; \
+		cp -rp ../../debian/ .; \
+		ln -s ../../Makefile;\
+		ln -s ../../src;\
+		ln -s ../../libhttpd;\
+		ln -s ../../resources;\
+		dpkg-buildpackage -b -us -uc
+	rm -rf dist/nodogsplash
---
+++ src/debug.c
@@ -0,0 +1,82 @@
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+/* $Id: debug.c 901 2006-01-17 18:58:13Z mina $ */
+/** @file debug.c
+    @brief Debug output routines
+    @author Copyright (C) 2004 Philippe April <papril777@yahoo.com>
+*/
+
+#include <stdio.h>
+#include <errno.h>
+#include <syslog.h>
+#include <stdarg.h>
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include "conf.h"
+
+/** @internal
+Do not use directly, use the debug macro */
+void
+_debug(char *filename, int line, int level, char *format, ...)
+{
+	char buf[28];
+	va_list vlist;
+	s_config *config = config_get_config();
+	time_t ts;
+	sigset_t block_chld;
+
+	time(&ts);
+
+	if (config->debuglevel >= level) {
+		sigemptyset(&block_chld);
+		sigaddset(&block_chld, SIGCHLD);
+		sigprocmask(SIG_BLOCK, &block_chld, NULL);
+
+		if (level <= LOG_WARNING) {
+			fprintf(stderr, "[%d][%.24s][%u](%s:%d) ", level, ctime_r(&ts, buf), getpid(),
+					filename, line);
+			va_start(vlist, format);
+			vfprintf(stderr, format, vlist);
+			va_end(vlist);
+			fputc('\n', stderr);
+		} else if (!config->daemon) {
+			fprintf(stdout, "[%d][%.24s][%u](%s:%d) ", level, ctime_r(&ts, buf), getpid(),
+					filename, line);
+			va_start(vlist, format);
+			vfprintf(stdout, format, vlist);
+			va_end(vlist);
+			fputc('\n', stdout);
+			fflush(stdout);
+		}
+
+		if (config->log_syslog) {
+			openlog("nodogsplash", LOG_PID, config->syslog_facility);
+			va_start(vlist, format);
+			vsyslog(level, format, vlist);
+			va_end(vlist);
+			closelog();
+		}
+
+		sigprocmask(SIG_UNBLOCK, &block_chld, NULL);
+	}
+}
